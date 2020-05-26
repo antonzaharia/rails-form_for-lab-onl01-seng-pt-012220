723cf15b4a5d5c7f9894a1529f91518d49bd06bf
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-05-26 15:32:30 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,2 +1,21 @@
 class StudentController < ApplicationController
+  def index
+    @student = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
