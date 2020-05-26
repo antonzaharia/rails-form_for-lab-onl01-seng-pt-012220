1cccaa978a3cbb77ef3256b6353239cd34bcff36
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-05-26 16:26:08 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,7 +12,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(post_params)
+    @school_class = SchoolClass.new(post_params(:title, :room_number))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
