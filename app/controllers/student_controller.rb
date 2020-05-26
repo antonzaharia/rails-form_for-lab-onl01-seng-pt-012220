5b27c27a6f29b48aca00ce0c072465155060a67d
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-05-26 15:37:22 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -23,6 +23,7 @@ class StudentController < ApplicationController
   def update
     @student = Student.find(params[:id])
     @student.update(post_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   private
